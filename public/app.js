ead44f0599e871d926774060db8bef8b2e62d687
Paulo Camopy <paulorenato.cpb@gmail.com>
2022-07-23 23:29:12 -0300

    feat: :sparkles: added basic frontend

---
+++ public/app.js
@@ -0,0 +1,29 @@
+window.addEventListener("DOMContentLoaded", (_) => {
+  let websocket = new WebSocket("ws://" + window.location.host + "/websocket");
+  let room = document.getElementById("chat-text");
+
+  websocket.addEventListener("message", function (e) {
+    let data = JSON.parse(e.data);
+    // creating html element
+    let p = document.createElement("p");
+    console.log(data);
+    p.innerHTML = `<strong>${data.userName}</strong>: ${data.text}`;
+
+    room.append(p);
+    room.scrollTop = room.scrollHeight; // Auto scroll to the bottom
+  });
+
+  let form = document.getElementById("input-form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let username = document.getElementById("input-username");
+    let text = document.getElementById("input-text");
+    websocket.send(
+      JSON.stringify({
+        user_name: username.value,
+        message: text.value,
+      })
+    );
+    text.value = "";
+  });
+});

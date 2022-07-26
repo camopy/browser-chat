540fc722618b02c39861dda8ef34d3a26319da9d
Paulo Camopy <paulorenato.cpb@gmail.com>
2022-07-26 16:53:54 -0300

    build: added docker shared volume for code files
    better dev experience, so I don't need to rebuild everytime

--- docker/app/bin/init.sh
+++ docker/app/bin/init.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
-echo 'Runing migrations...'
-/browser-chat/bin/migrate up > /dev/null 2>&1 &
+if $DEV; then
+  echo 'Runing migrations...'
+  go run /browser-chat/cmd/migrate/main.go up > /dev/null 2>&1 &
 
-echo 'Start application...'
-/browser-chat/bin/app
+  echo 'Start application...'
+  go run /browser-chat/cmd/app/main.go
+else
+  echo 'Runing migrations...'
+  /browser-chat/bin/migrate up > /dev/null 2>&1 &
+
+  echo 'Start application...'
+  /browser-chat/bin/app
+fi
